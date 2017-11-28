8815f17c224f991815c23eb2aa2a5613d98fe35b
ZhanTewei <zhantewei@gmail.com>
2017-11-28 10:32:46 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ container.innerHTML=`
     <span>html</span>
     <div>pagination</div>
     <button class="btn" id="btn"> btn</button>
-    <button class="btn" onclick="btn2(event)">btn2</button>
+    <button class="btn" onclick="btn2(event)">btn2</button>                                                                                                                                                                                                                                                                                                                                                                                                
 `;
 const btn=document.getElementById('btn');
 btn.addEventListener('click',()=>{
@@ -20,10 +20,20 @@ btn.addEventListener('click',()=>{
 Render(`
     <div>html</div>
     <div>
-        image
+        image{{}}
         <input required class="form-control" type="number" max="5" min="1">
     </div>
 `,container);
+const readonly=function(n:string):any{
+    return (target:any,name:any,descriptor:any)=>{
+        console.log(target,name)
+    }
+};
 
+
+class Person{
+    @readonly('n')name:any;
+}
+const person=new Person();
 
 
